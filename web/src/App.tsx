099deb06c704e50182c32385978653dcbfb14834
Guilherme Ziolle <gziolle@gmail.com>
2022-02-01 20:57:13 -0300

    add graphql-codegen code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,11 @@
 import { gql, useQuery } from "@apollo/client";
+import { useHelloQuery } from "./generated/graphql";
 
 function App() {
-  const { data, loading } = useQuery(gql`
-    {
-      hello
-    }
-  `);
+  const { data, loading } = useHelloQuery();
+
   if (loading) return <div>loading...</div>;
-  return <div>{JSON.stringify(data)}</div>;
+  return <div>{JSON.stringify(data?.hello)}</div>;
 }
 
 export default App;
