e78b074b6a01732433aa0d1a6c08dbf1ccd4eadd
Guilherme Ziolle <gziolle@gmail.com>
2022-02-01 21:49:06 -0300

    add register form and mutation

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,5 +1,47 @@
-interface Props {}
+import { useState } from "react";
+import { useRegisterMutation } from "../generated/graphql";
+import { Navigate, useNavigate } from "react-router-dom";
 
-export const Register: React.FC<Props> = () => {
-    return <div>register page</div>
-}
+export const Register: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [registerUser] = useRegisterMutation();
+  const navigate = useNavigate();
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log("form submitted");
+
+        const response = await registerUser({
+          variables: {
+            email,
+            password,
+          },
+        });
+
+        console.log(response);
+        navigate("/");
+      }}
+    >
+      <div>
+        <input
+          type="email"
+          value={email}
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">register</button>
+    </form>
+  );
+};
