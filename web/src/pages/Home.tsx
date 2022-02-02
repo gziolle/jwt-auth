929d4a6d76f2ba5a66802922ae8c1ea18550353b
Guilherme Ziolle <gziolle@gmail.com>
2022-02-01 21:56:24 -0300

    display registered users in the home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import { useUsersQuery } from "../generated/graphql";
+
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
-    return <div>home page</div>
-}
+  const { data, loading } = useUsersQuery({ fetchPolicy: "network-only" });
+
+  if (!data || loading) {
+    return <div>loading...</div>;
+  }
+  return (
+    <div>
+      <div>users:</div>
+      <ul>
+        {data.users.map((user) => (
+          <div key={user.id}>
+            <div>{user.id}, {user.email}</div>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
